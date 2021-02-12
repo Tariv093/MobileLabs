f8a161c0c7d96e35e6d720768dc3d14dbeeeb4b9
Tariv093 <tariv093@gmail.com>
2021-02-12 00:04:08 -0500

    Lab3 Commit

---
+++ GAME3004-W2021-Lab2b-main/Assets/_Scripts/EnemyScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class EnemyScript : MonoBehaviour
+{
+    public NavMeshAgent navmesh;
+    public PlayerBehaviour player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        navmesh = GetComponent<NavMeshAgent>();
+        player = FindObjectOfType<PlayerBehaviour>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        navmesh.SetDestination(player.transform.position);
+        //line of sight 
+        
+    }
+}
